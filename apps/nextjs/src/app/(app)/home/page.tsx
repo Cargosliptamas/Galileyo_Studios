7ba79b68f96e20ab56539a4bc555ed4bc3511ec9
andriskaaz <andras@cargoslip.one>
2025-07-30 19:44:24 +0200

    feat: home page route

---
+++ apps/nextjs/src/app/(app)/home/page.tsx
@@ -0,0 +1,12 @@
+import HomePageComponent from "~/components/public-site/home";
+import {
+  HydrateClient,
+} from "~/trpc/server";
+
+export default function HomePage() {
+  return (
+    <HydrateClient>
+      <HomePageComponent />
+    </HydrateClient>
+  );
+}
